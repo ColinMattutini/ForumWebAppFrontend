eeb3fd0f28b7946bc7df40faf4d1330e0ed84e9b
Colin <cmmatt14@gmail.com>
2022-11-03 20:06:45 -0400

    modal and modal portal added

---
+++ socialmediafrontend/src/UI/Modal.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import classes from './Modal.module.css';
+import ReactDOM from "react-dom";
+
+
+const ModalCard = (props) => {
+    return(
+        <div className={classes.modal}>
+            {props.children}
+        </div>
+    )
+}
+
+const Backdrop = (props) => {
+    return(
+        <div className={classes.backdrop}>
+        </div>
+        
+    )
+}
+
+const portalElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+
+    return(
+        <Fragment>
+            {ReactDOM.createPortal(<ModalCard>{props.children}</ModalCard>, portalElement)}
+            {ReactDOM.createPortal(<Backdrop />, portalElement)}
+        </Fragment>
+    );
+
+}
+
+export default Modal;
