e16a81c2b1802df7c11153d98ec42f89d05d1170
Colin <cmmatt14@gmail.com>
2022-11-06 11:20:18 -0500

    hide modal on clicking background

--- socialmediafrontend/src/components/Post/PostList.js
+++ socialmediafrontend/src/components/Post/PostList.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Card from '../../UI/Card';
 import Modal from '../../UI/Modal';
 import IndividualPost from './IndividualPost';
@@ -6,18 +6,29 @@ import IndividualPost from './IndividualPost';
 const PostList = (props) => {
 
     const [showPostModal, setShowPostModal] = useState(false);
+
     
 
     const showPostModalHandler = () => {
         setShowPostModal(true);
     }
 
-    
+    const hidePostModalHandler = () => {
+        setShowPostModal(false);
+    }
+
+    useEffect(() => {
+        const body = document.querySelector('body');
+        body.style.overflow = showPostModal ? 'hidden' : 'auto';
+      }, [showPostModal])
 
     return(
         <Fragment>
             {showPostModal && <IndividualPost 
+                hidePostModalHandler = {hidePostModalHandler}
                 postName = {props.postName}
+                postDescription = {props.postDescription}
+                postId = {props.postId}
             />}
             <Card onClick={showPostModalHandler}>
                 <p>{props.postUser}</p>
