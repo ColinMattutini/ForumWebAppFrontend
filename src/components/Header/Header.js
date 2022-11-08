ec84247dbe440b0a218401bf01442a429b0f12b9
Colin <cmmatt14@gmail.com>
2022-11-08 18:09:00 -0500

    replaced showing login modal with context

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import AuthContext from '../../Context/userauth';
 import Login from '../../UserAuthentication/Login';
+import SignUpForm from '../../UserAuthentication/SignupForm.js';
 import SearchBar from '../SearchBar/SearchBar';
 import classes from './Header.module.css';
 
 const Header = () => {
 
     const navigate = useNavigate();
+    const authCtx = useContext(AuthContext);
 
-    const[showLogin, setShowLogin] = useState(false);
+    // const [showLogin, setShowLogin] = useState(false);
+    const [showSignUp, setShowSignUp] = useState(false);
 
-    const showLoginModalHandler = () => {
-        setShowLogin(true);
+    // const showLoginModalHandler = () => {
+    //     setShowLogin(true);
+    // }
+
+    // const hideLoginModalHandler = () => {
+    //     setShowLogin(false);
+    // }
+
+    const showSignUpModalHandler = () => {
+        setShowSignUp(true);
     }
 
-    const hideLoginModalHandler = () => {
-        setShowLogin(false);
+    const hideSignupModalHandler = () => {
+        setShowSignUp(false);
     }
 
     const homepageNav = () => {
         navigate("");
     }
 
+    useEffect(() => {
+
+    }, [authCtx.isLoggedIn])
+
     return(
         <div>
-        {showLogin && <Login hideLoginModalHandler={hideLoginModalHandler}/>}
+        {authCtx.loginModal && <Login showSignUpModalHandler={showSignUpModalHandler}/>}
+        {showSignUp && <SignUpForm hideSignupModalHandler={hideSignupModalHandler} />}
         <header className={classes.header}>
-          
                 <h1 onClick={homepageNav}>Hobby Forum</h1>
                 <SearchBar />
                 <div>
-                <button>Sign Up</button>
-                <button onClick={showLoginModalHandler}>Log in</button>
+                {!authCtx.isLoggedIn && <button onClick={showSignUpModalHandler}>Sign Up</button>}
+                {!authCtx.isLoggedIn && <button onClick={authCtx.showLoginModal}>Log in</button>}
+                {authCtx.isLoggedIn && <button onClick={authCtx.logout}>Logout</button>}
                 </div>
         </header>
         </div>
