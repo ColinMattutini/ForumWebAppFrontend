c25538d887588c4c2fa064152e2388d7b54d4793
Colin <cmmatt14@gmail.com>
2022-11-08 07:13:51 -0500

    display comment count

--- socialmediafrontend/src/components/Post/IndividualPost.js
+++ socialmediafrontend/src/components/Post/IndividualPost.js
@@ -7,6 +7,15 @@ import PostScore from "./PostScore";
 
 const IndividualPost = (props) => {
 
+  const [commentCount, setCommentCount] = useState(0);
+  const [noCommentDisplay, setNoCommentDisplay] = useState(false);
+
+  const commentCounterHandler = (comments) => {
+    setCommentCount(comments);
+    if(comments === 0){
+      setNoCommentDisplay(true);
+    }
+  }
     
     useEffect(() => {
         function handleEscapeKey(e) {
@@ -19,6 +28,8 @@ const IndividualPost = (props) => {
         return () => document.removeEventListener('keydown', handleEscapeKey)
       }, [])
     
+    
+
     return(
         
             <Modal hidePostModalHandler={props.hidePostModalHandler}>
@@ -29,12 +40,17 @@ const IndividualPost = (props) => {
                     {/* Username of  poster */}
                     <h1>{props.postName}</h1>
                     <h3>{props.postDescription}</h3>
+                    <h2>{commentCount} comments</h2>
                     {/* post score on bottom right positive and negative */}
                     <PostScore postId={props.postId}/>
                     
+                    
                 </div>
-                
-                <Comment postId={props.postId}/>
+                {noCommentDisplay && <h1>No Comments Yet!</h1>}
+                <Comment 
+                  postId={props.postId}
+                  commentCounterHandler={commentCounterHandler}
+                />
             </Modal>
         
       
