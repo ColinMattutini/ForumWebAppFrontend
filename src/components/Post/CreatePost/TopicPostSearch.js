f0fa28ae549e7d14ded801fcf485f7552d1241f2
Colin <cmmatt14@gmail.com>
2022-11-08 18:50:43 -0500

    search for a topic when posting

---
+++ src/components/Post/CreatePost/TopicPostSearch.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useRef, useState } from "react";
+import classes from "./TopicPostSearch.module.css";
+
+const TopicPostSearch = (props) => {
+
+const [search, setSearch] = useState("");
+const [startSearch, setStartSearch] = useState(true);
+const inputRef = useRef();
+const placeholder = ("Search: \"Cooking\"");
+const topics = [
+    {name: "Cooking"},
+    {name: "Fitness"},
+    {name: "Sports"},
+    {name: "Knitting"},
+    {name: "Programming"}
+
+]
+
+const onChangeSearch = e => {
+    setSearch(e.target.value);
+    setStartSearch(true);
+}
+
+
+const topicSearch = topics.filter(topic => {
+    if (search === "" || !startSearch) {
+      return "";
+    } else if (topic.name.toLowerCase().includes(search.toLowerCase())) {
+      return topic;
+    }
+  }).map((topic) => (
+    <div className={classes.button} key={topic.id}>
+      <button onClick={(e) => 
+        {
+            e.preventDefault();
+            inputRef.current.value = topic.name;
+            props.topicChoiceHandler(topic.name);
+            setSearch(topic.name);
+            setStartSearch(false);
+            
+        }}>{topic.name}</button>
+    </div>
+  ));
+
+return(
+    <Fragment>
+        <div className={classes.position}>
+        <label>Select a topic to post to: </label>
+        <div className={classes.search}>
+            <input type="text" placeholder={placeholder}  ref={inputRef} onChange={onChangeSearch} value={search}/>
+        </div>
+            <div className={classes.dropdown}>
+                {topicSearch}
+            </div>
+        </div>
+            </Fragment>  
+        
+    
+    )
+}
+
+export default TopicPostSearch;
