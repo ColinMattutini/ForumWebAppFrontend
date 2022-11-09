f872e8d7edf64e363e259b0b1e1d79d2f3ffc914
Colin <cmmatt14@gmail.com>
2022-11-08 21:22:01 -0500

    login modal styling and swap to signup link

--- src/UserAuthentication/Login.js
+++ src/UserAuthentication/Login.js
@@ -1,15 +1,20 @@
-import React, { useRef } from "react";
-import Modal from "../UI/Modal";
+import React, { useContext, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../Context/userauth";
+import LoginModal from "../UI/LoginModal";
 import classes from "./Login.module.css";
 
 const Login = (props) => {
 
     const email = useRef();
     const password = useRef();
+    const navigate = useNavigate();
+    const authCtx = useContext(AuthContext);
 
     const userLogin = (email, password) => {
         fetch(
-            'https://hobby-forum.herokuapp.com/api/login',
+            // 'https://hobby-forum.herokuapp.com/api/login',
+            "http://localhost:8080/api/login",
             {
                 method: 'POST',
                 headers: {
@@ -20,10 +25,7 @@ const Login = (props) => {
                     username: email,
                     password: password
                 })
-                    
-                    
-                
-                
+
             }
             
             ).then(res => {
@@ -45,7 +47,9 @@ const Login = (props) => {
                   console.log(data);
                 //   authCtx.login(data.access_token, data.username);
                 localStorage.setItem("token", data.access_token);
-                  localStorage.setItem("username", data.username);
+                  localStorage.setItem("email", data.email);
+                 authCtx.login();
+                  
                   //authCtx.id(data.username);
                 //   navigate('/homepage');
                   
@@ -54,40 +58,51 @@ const Login = (props) => {
                   .catch(error => {
                       alert('Something went wrong');
                 });
-
+                
             };
 
     const onLogin = e => {
         e.preventDefault();
-        console.log(email.current.value, password.current.value);
         userLogin(email.current.value, password.current.value);
+        authCtx.hideLoginModal();
+        navigate("");
+    }
+
+    const switchToSignup = () => {
+        authCtx.hideLoginModal();
+        props.showSignUpModalHandler();
     }
 
     return(
-    <Modal>
-        <button onClick={props.hideLoginModalHandler}>X</button>
-        <form onSubmit={onLogin}>
+    
+    <LoginModal>
+        <div className={classes.closeButton}>
+            <button onClick={authCtx.hideLoginModal}>X</button>
+        </div>
+        <form onSubmit={onLogin} className={classes.login}>
+                <h1>Login</h1>
                 <div className={classes.signupControl}>
-                <label htmlFor='email'>E-Mail</label>
+                <label htmlFor='email'></label>
                 <input 
+                placeholder="Email"
                     type='text'
                     ref={email}
                     
                 />
-                <label htmlFor='password'>Password</label>
+                <label htmlFor='password'></label>
                 <input 
+                    placeholder="Password"
                     type='password'
                     ref={password}
                 />                
-                
-                {/* <button onClick={props.hideLoginForm}>Close</button> */}
-                <button>Cancel</button>
-                <p>Don't have an account? Sign-up here!</p>
+                    <button>Login</button>
+                <p onClick={switchToSignup}>Don't have an account? Sign-up here!</p>
                 </div>
-                <button>Submit</button>
+                
             </form>
             
-    </Modal>
+            </LoginModal>
+    
     )
 }
 
