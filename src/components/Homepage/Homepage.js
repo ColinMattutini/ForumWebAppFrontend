3f21267ef720abc2293706464bc41b009c5c11c7
Colin <cmmatt14@gmail.com>
2022-11-11 17:05:18 -0500

    removed wrong import

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import AllPosts from '../Post/AllPosts';
-import FavoriteTopics from '../Topic/FavoriteTopics';
 import classes from "./Homepage.module.css";
 
 const Homepage = () => {
