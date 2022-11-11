0779e1f0bad149e67a00598cfaf5d700b0ba3616
Colin <cmmatt14@gmail.com>
2022-11-10 21:40:24 -0500

    global timeout state

--- src/Context/userauth.js
+++ src/Context/userauth.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AuthContext = React.createContext({ 
     isLoggedIn: false,
     loginModal: false,
+    postActionTimeout: false,
+    commentActionTimeout: false,
     logout: () => {},
     login: () => {},
     showLoginModal: () => {},
-    hideLoginModal: () => {}
-    
+    hideLoginModal: () => {},
+    posttimeoutHandler: () => {},
+    commentTimeoutHandler: () => {}
 });
 
 export const AuthContextProvider = (props) => {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [showLoginModal, setShowLoginModal] = useState(false);
+    const [postActionTimeout, postSetActionTimeout] = useState(false);
+    const [commentActionTimeout, setCommentActionTimeout] = useState(false);
+    
 
     const logoutHandler = () => {        
         localStorage.clear();
@@ -33,13 +40,33 @@ export const AuthContextProvider = (props) => {
         setShowLoginModal(false);
     }
 
+    const posttimeoutHandler = () => {
+        postSetActionTimeout(true);
+        setTimeout(() => {
+           
+            postSetActionTimeout(false);
+         }, 1000)
+    }
+
+    const commentTimeoutHandler = () => {
+        setCommentActionTimeout(true);
+        setTimeout(() => {
+           
+            setCommentActionTimeout(false);
+         }, 5000)
+    }
+
     const contextValue = {
         isLoggedIn: isLoggedIn,
         loginModal: showLoginModal,
+        postActionTimeout: postActionTimeout,
+        commentActionTimeout: commentActionTimeout,
         logout:logoutHandler,
         login:loginHandler,
         showLoginModal:loginModalHandler,
-        hideLoginModal:hideLoginHandler
+        hideLoginModal:hideLoginHandler,
+        posttimeoutHandler: posttimeoutHandler,
+        commentTimeoutHandler: commentTimeoutHandler
     };
 
     return(
