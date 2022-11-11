c62b9135c44b5a82debbf74a1784e531ef71453b
Colin <cmmatt14@gmail.com>
2022-11-10 21:39:15 -0500

    create post req and timeout

--- src/components/Post/CreatePost/CreatePost.js
+++ src/components/Post/CreatePost/CreatePost.js
@@ -1,14 +1,18 @@
 import Modal from "../../../UI/Modal";
-import React, { useRef, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import classes from "./CreatePost.module.css"
 import SearchBar from "../../SearchBar/SearchBar";
 import TopicPostSearch from "./TopicPostSearch";
+import AuthContext from "../../../Context/userauth";
 
 const CreatePost = (props) => {
     
     const [topicChoice, setTopicChoice] = useState("");
+    const [timeoutPost, setTimeoutPost] = useState(false);
     const postNameRef = useRef();
     const postDescriptionRef = useRef();
+
+    const authCtx = useContext(AuthContext);
 
     const topicChoiceHandler = (choice) => {
         setTopicChoice(choice);
@@ -30,10 +34,19 @@ const CreatePost = (props) => {
         )
     }
 
+    // const postTimeoutHandler = () => {
+    //     setTimeout(() => {
+    //         setTimeoutPost(false);
+           
+    //      }, 100000)
+    // }
+
     const submitPostHandler = (e) => {
         e.preventDefault();
         submitPostFetch(postNameRef.current.value, postDescriptionRef.current.value);
-        
+        authCtx.posttimeoutHandler();
+        console.log("timeout");
+        // props.hideCreatePostHandler();
     }
 
     return(
@@ -41,15 +54,21 @@ const CreatePost = (props) => {
             <button onClick={props.hideCreatePostHandler}>X</button>
             <form onSubmit={submitPostHandler}>
                 <div className={classes.inputholder}>
-                    <textarea cols="30" rows="2" placeholder="Post Title" maxLength={200} ref={postNameRef}></textarea>  
+                    <textarea cols="30" rows="2" placeholder="Post Title" maxLength={200} ref={postNameRef} required></textarea>  
                 </div>
                 
                 <div className={classes.postDescription}>
-                    <textarea cols="30" rows="10" placeholder="Post Description" ref={postDescriptionRef}></textarea>
+                    <textarea cols="30" rows="10" placeholder="Post Description" ref={postDescriptionRef} required></textarea>
                 </div>
                 <TopicPostSearch topicChoiceHandler={topicChoiceHandler}/>
+                
+                
+                  
                 <div className={classes.submitButton}>
-                    <button>Submit</button>
+                <button disabled={authCtx.postActionTimeout}>Submit</button> 
+                
+                    
+                
                 </div>
                 
             </form>
